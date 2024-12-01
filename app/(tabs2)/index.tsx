1e253ad560cf249477f765a7bbc9930b5ec04dc7
sk-py <shaikh56742@gmail.com>
2024-12-02 00:01:17 +0530

    Tabs configuring --pending

--- app/(tabs2)/index.tsx
+++ app/(tabs2)/index.tsx
@@ -1,7 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export const index = () => {
+const index = () => {
   return (
-    <div>index</div>
+    <View>
+      <Text>index</Text>
+    </View>
   )
 }
+
+export default index
+
+const styles = StyleSheet.create({})
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,27 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack, Tabs } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack, Tabs } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/useColorScheme";
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -23,7 +29,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -49,10 +55,19 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Tabs>
-        <Tabs.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Tabs.Screen name="+not-found" options={{href:null}} />
+        <Tabs.Screen
+          name="(tabs)"
+          options={{
+            tabBarIcon: ({ focused }) => {
+              return <Ionicons name={`${focused ? "home" : "home-outline"}`} color={"#FFFFFF"} size={24} />;
+            },
+            tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen name="+not-found" options={{ href: null }} />
       </Tabs>
     </ThemeProvider>
   );
